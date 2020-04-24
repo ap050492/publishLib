c665e9ebd9458f0adfda7af64fe034ab35569b3e
amit padmani <amit.padmani@ayanworks.com>
2020-04-24 16:59:06 +0530

    first commit

---
+++ src/main/java/com/ayanworks/aries_sdk_java/MainModule.java
@@ -0,0 +1,10 @@
+package com.ayanworks.aries_sdk_java;
+
+import android.util.Log;
+
+public class MainModule {
+    public static String createWalletAndDidDemo(String walletName, String key) {
+        Log.d("PARAMETERS", walletName.toString());
+        return "HJHAISUsdh32jjkds8098k";
+    }
+}
